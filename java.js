62a166c6e2dcb3decf49755ab78daf748f92b641
Basil <basilmosa99@gmail.com>
2019-10-03 13:16:53 +0300

    edit

--- java.js
+++ java.js
@@ -1,9 +1,24 @@
-var property=prompt("choose number from 0 & 23");
-if(property == 'yes'){
-    alert("You had a nice experiment");
-}else if(property=='no'){
-    alert("I advice you to buy one");
-}
-else {
-    alert("I can't help you");
-    }
+ 
+            
+
+    
+    var showorder=function () {
+        //input
+        var userorder = prompt ('What do you prefer nissan or BM?');
+        var orderimage='';
+        var numberofimages;
+        //processing
+        while (userorder !== 'Mercedes' && userorder !== 'BM') {
+            userorder = ('What is the most expensive Mercedes or Nissan');
+            }
+            numberofimages = prompt('how many times do you want to see?');
+            for (let ctr =0;ctr < numberofimages; ctr++) {
+                if (userorder === 'BM') {
+                    orderimage +='<img class="style" src="img/bmw.jpg">';
+                } else if (userorder === 'TOYOTA'){
+                    orderimage=orderimage + '<img class="style" src="img/m.jpg">';
+            }
+        }
+        //output
+        return orderimage;
+     };
