e96fcb61f244e9393f033874c7b0c7556370854e
Basil <basilmosa99@gmail.com>
2019-10-03 09:07:34 +0300

    edit

---
+++ java.js
@@ -0,0 +1,9 @@
+var property=prompt("choose number from 0 & 23");
+if(property == 'yes'){
+    alert("You had a nice experiment");
+}else if(property=='no'){
+    alert("I advice you to buy one");
+}
+else {
+    alert("I can't help you");
+    }
